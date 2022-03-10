e0bedf1ea0aa6f39feb673b408bc694812458fb5
Baharuddin Uyun <baharuddin.uyun.2016@ft.umy.ac.id>
2022-03-10 18:50:18 +0700

    Passing Parameters

--- app/src/main/java/com/example/sample_table_layout/MainActivity.java
+++ app/src/main/java/com/example/sample_table_layout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sample_table_layout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,16 +31,32 @@ public class MainActivity extends AppCompatActivity {
                 nama = eduser.getText().toString(); //mindahin inputan dari Edittext ke String
                 password = edpassword.getText().toString();
 
-                if(nama.equals("uyun") && password.equals("123")){
-                    Toast t = Toast.makeText(getApplicationContext(), "Login Sukses!", Toast.LENGTH_LONG); //lengthlong?
+                String sUser = "admin";
+                String sPass = "123";
+
+                if(nama.isEmpty() || password.isEmpty()){
+                    // bikin bubble message
+                    Toast t = Toast.makeText(getApplicationContext(), "Username dan password wajib diisi!", Toast.LENGTH_LONG); //lengthlong?
                     t.show();
                 }else {
-                    Toast b = Toast.makeText(getApplicationContext(), "Username atau Password salah!",Toast.LENGTH_LONG);
-                    b.show();
+                    if(nama.equals(sUser) && password.equals(sPass)) {
+                        Toast t = Toast.makeText(getApplicationContext(), "Login Sukses!", Toast.LENGTH_LONG); //lengthlong?
+                        t.show();
+                        // object bundle?
+                        Bundle b = new Bundle();
+//                        //masukin string ke bundle untuk dikirim ke layout/activity lain dengan key yang ada
+                        b.putString("a", nama.trim());
+                        b.putString("b", password.trim());
+//                        //intent
+                        Intent i = new Intent(MainActivity.this,ActivityKedua.class);
+//                        //masukin bundle dan intent ke activity lain;
+                        i.putExtras(b);
+                        startActivity(i);
+                    }else{
+                        Toast b = Toast.makeText(getApplicationContext(), "Username atau Password salah!", Toast.LENGTH_LONG);
+                        b.show();
+                    }
                 }
-
-                // bikin bubble message
-
 
             }
         });
